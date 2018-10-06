bfd6b151d2cf9266ef09cc37480a8dbcdf4f0c6e
HUbert Strumiński <hubert.struminski@gmail.com>
2018-10-06 19:44:36 +0200

    add function getAnswerByQuestion() in ESProvider class -> store data after input on answer in List

--- ESProvider.java
+++ ESProvider.java
@@ -1,4 +1,6 @@
 import java.util.List;
+import java.util.Scanner;
+import java.util.ArrayList;
 
 public class ESProvider{
 
@@ -14,14 +16,38 @@ public class ESProvider{
 
     }
 
-    public void getAnswerByQuestion(String questionId){
+    public List<String> getAnswerByQuestion(){
+        Scanner scanner = new Scanner(System.in);
         RuleParser ruleParser = new RuleParser();
+        SingleValue singleValue = new SingleValue();
+        MultipleValue multipleValue = new MultipleValue();
         RuleRepository ruleRepository = ruleParser.getRuleRepository();
+        List<String> singleValueList = singleValue.getSingleValueAnswer();
+        List<String> multipleValueList = multipleValue.getMultipleValueAnswer();
         List<String> questionList = ruleRepository.getQuestionList();
+        String input;
+        List<String> answersList = new ArrayList<>();
 
-        for(String element: questionList){
-            System.out.println(element);
+        for(int i=0; i<questionList.size(); i++){
+            System.out.println(questionList.get(i));
+            if(i == 0){
+                System.out.println(singleValueList.get(0) + " OR " + singleValueList.get(1));
+                System.out.print("Enter answer: ");
+                input = scanner.next();
+                answersList.add(input);
+            }else if(i == 1){
+                System.out.println(singleValueList.get(2) + " OR " + singleValueList.get(3));
+                System.out.print("Enter answer: ");
+                input = scanner.next();
+                answersList.add(input);
+            }else if(i == 2){
+                System.out.println(multipleValueList.get(0) + " OR " + multipleValueList.get(1));
+                System.out.print("Enter answer: ");
+                input = scanner.next();
+                answersList.add(input);
+            }
         }
+        return answersList;
     }
 
     public static void main(String[] args){
@@ -30,6 +56,6 @@ public class ESProvider{
 
         ESProvider esProvider = new ESProvider(factParser, ruleParser);
 
-        esProvider.getAnswerByQuestion("");
+        List<String> answerList = esProvider.getAnswerByQuestion();
     }
 }
